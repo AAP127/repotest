cdd90ff0011a2dff93be235065c5f7fabbc5ae72
AAP127 <apestonj@gmail.com>
2022-02-04 11:52:15 -0800

    Added NumFunction.java and test

---
+++ NumFunction.java
@@ -0,0 +1,11 @@
+class NumFunction{
+
+    public static int numReturn(){
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numReturn());
+    }
+
+}
---
+++ NumFunctionTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class NumFunctionTest {
+    
+    @Test
+    public void testNums(){
+        assertEquals(2, NumFunction.numReturn());
+    }
+
+
+}
